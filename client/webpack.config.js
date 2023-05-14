94c8b2af2a857fb28347ac8e11bcda8630c98305
jd479800 <jd479800@gmail.com>
2023-05-14 14:03:13 -0400

    Adds CSS loaders and babel to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -22,7 +22,26 @@ module.exports = () => {
     ],
 
     module: {
+      // Adds CSS loaders and babel to webpack
       rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+      ],
+    },
         
       ],
     },
